5e7f8c331e421af902f9b6324040177b57b24045
Karl Lew <karl@firepick.org>
2021-02-11 14:43:41 -0800

    v1.0.6 routerBase

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,16 @@
 import NuxtConfig from '@sc-voice/scv-static/nuxt.config';
 let CONFIG = JSON.parse(JSON.stringify(NuxtConfig));
+var { name } = require('./package.json');
+var appName = name.split('/').filter(n=>n.length).pop();
+var babelCompact;
+let BABEL_ENV = process.env.BABEL_ENV;
+let routerBase = BABEL_ENV === 'deploy' 
+    ? `/${appName}/`
+    : '/';
+babelCompact = BABEL_ENV === 'deploy' ? true : false;
+console.log(`nuxt.config.js`, JSON.stringify({routerBase, BABEL_ENV, babelCompact}));
+
+CONFIG.router.base = routerBase;
 Object.assign(CONFIG.head, {
     titleTemplate: '%s - static',
     title: 'Dhammaregen/sutta',
